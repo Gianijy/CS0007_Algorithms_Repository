ccfea5cb49cdf5cbbd3426253aaf3a2addbdef18
Gianijy <gianijyc.28@gmail.com>
2024-03-20 14:45:38 +0800

    Update sample4.cpp

--- Module 1/Basics/sample4.cpp
+++ Module 1/Basics/sample4.cpp
@@ -4,21 +4,31 @@ using namespace std;
 int main()
 {
 	int arr[5], i, min, max;
+
 	//input value
 	cout << "Enter five integers: ";
-	for (i=0; i<5; i++){
+
+	for(i = 0; i < 5; i++)
+	{
 		cin >> arr[i];
 	}	
+
 	//set the init value of min and max
 	min = arr[0];
 	max = arr[0];
-	for (i=0; i<5;i++){
-		if (min>arr[i])
+	for(i = 0; i < 5; i++)
+	{
+		if (min > arr[i])
+		{
 			min = arr[i];
-		if (max < arr[i])
+		}	
+		else if(max < arr[i])
+		{
 			max = arr[i];
+		}
+			
 	}
-	cout << "\nMAX value is  " << max;
-	cout << "\nMIN value is  " << min;
+	cout << "\nMAX value is:  " << max;
+	cout << "\nMIN value is:  " << min << endl;
 	return 0;
 }
